01ea731e85cf0dc256e79974a23940e94737d2f2
_GLDDRK_ <69928053+G0LDDARK@users.noreply.github.com>
2020-12-15 09:05:04 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import os
-__version__ = 1.9
-__author__ = "GLDDRK"
+﻿import os
+__version__ = str("1.9.2")
+__author__ = str("GLDDRK")
 def main():
     os.system("clear||cls")
-    print("En premier entre ta note, puis entre sur combien est ta note, ensuite le programme calculera ta note en note sur 20 \n(attention si vous n'entrer pas des nombres le script bugra vous devrez le relancer)")
+    print("En premier entre ta note, puis entre sur combien est ta note, ensuite le script calculera ta note en note sur 20 \n(Attention si vous n'entrer pas des nombres le script bugra vous devrez le relancer)")
     note = input("Quel est votre note: ")
     sur = input("Sur combient de point: ")
     # Калашников
--- note_aré.py
+++ note_aré.py
@@ -1,47 +1,57 @@
-#importation
+﻿#importation du module os
 import os
 
 #version
-__version__ = 1.9
+__version__ = str("1.9.2")
 
-#auteur
-__author__ = "GLDDRK"
+#Autheur
+__author__ = str("GLDDRK")
 
-#creation de la fonction main
+#fonction main()
 def main():
-    #explcation du programme + clear du terminal
+    #clear console
     os.system("clear||cls")
-    print("En premier entre ta note, puis entre sur combien est ta note, ensuite le programme calculera ta note en note sur 20 \n(attention si vous n'entrer pas des nombres le script bugra vous devrez le relancer)")
+
+    #expliction du script
+    print("En premier entre ta note, puis entre sur combien est ta note, ensuite le script calculera ta note en note sur 20 \n(Attention si vous n'entrer pas des nombres le script bugra vous devrez le relancer)")
 
     #demande de la note
     note = input("Quel est votre note: ")
 
-    #sur combien
+    #su combien est la note
     sur = input("Sur combient de point: ")
     # Калашников
 
-    #petit easter-egg
+    #easter-egg
     if note == "AUTHOR":
         if sur == "AUTHOR":
             print("Je suis " + str(__author__) + " l'auteur de ce code python, merci d'avoir demander qui je suis.")
             exit()
 
-    #si "VERSION" est dans les deux input afficher la version du progamme
+    #si les deux entrée son VERSION le script donne sa version
     if note == "VERSION":
         if sur == "VERSION":
             print("Mon autheur a dit que je suis en version " + str(__version__))
             exit()
 
-    #affichage de la note
+    #calcule de la note
     notesur20 = float(note) / float(sur) * 20
+
+    #affichage de la note
     print("Votre note de " + str(note) + " sur " + str(sur) + " est égale a la note de " + str(notesur20) + "/20")
 
-    #demande de recommencement
+    #damande si il vet recommancer
     restart = input("Voulez vous recommencer? (O/n)?")
+
+    #si oui recomancer
     if restart == "O":
         os.system("clear||cls")
         main()
+
+    #sinon fermer le script
     else:
         os.system("clear||cls")
         exit()
+
+#execution fonction main()
 main()
